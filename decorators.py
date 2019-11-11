3697484414bba8f8f3cca2d0b0d707e5376042ce
imran <imrankhannn81@gmail.com>
2019-11-11 18:49:19 +0500

    decorators

---
+++ decorators.py
@@ -0,0 +1,20 @@
+# def outer_function(msg):
+#
+#     def inner_function():
+#         print(msg)
+#     return inner_function
+
+
+
+
+def decorator_function(original_function):
+    def wrapper_function():
+        return original_function()
+    return wrapper_function
+
+def display():
+    print('display function ran')
+
+decorated_display=decorator_function(display)
+
+decorated_display()
