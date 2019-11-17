db5b53dfbbfb1d6ce5f3eff2619f16cd01b5d556
imran <imrankhannn81@gmail.com>
2019-11-17 23:42:33 +0500

    string formatting

---
+++ string_formating.py
@@ -0,0 +1,56 @@
+#person={'name':'Jenn','age':23}
+
+#sentence='my name is {0[name]} and i am {0[age]} years old'.format(person)
+
+#print(sentence)
+
+#tag='h1'
+#text='this is a headline'
+
+#sentence='<{0}>{1}</{0}>'.format(tag,text)
+
+#print(sentence)
+
+# class Person():
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#
+#
+# p1=Person('jack',24)
+#
+# sentence='my name is {0.name} and I am {0.age} years old.'.format(p1)
+# print(sentence)
+#
+# sentence='my name is {name} and I am {age} years old.'.format(name='Jenn',age='30')
+# print(sentence)
+
+# person={'name':'jenn','age':23}
+# sentence='my name is {name} and i am {age} years old'.format(**person)
+# print(sentence)
+
+
+# for i in range(1,11):
+#     sentence='the value is {1:03}'.format(i)
+#     print(sentence)
+
+# pi=3.141529265
+#
+# sentence='pi is equal to {0:.10f}'.format(pi)
+# print(sentence)
+#
+# sentence='1 mb is equal to {:,.2f} bytes'.format(1000**2)
+# print(sentence)
+
+# import datetime
+# my_date=datetime.datetime()
+# print(my_datetime)
+#
+# sentence='{:%B %d %Y}'.format(my_date)
+# print(sentence)
+
+# import datetime
+# my_date=datetime.datetime(2016,9,24,12,30,45)
+#
+# sentence='{0:%B %d %Y} fell on a {0:%A} and was te {0:%j} day of the year'.format(my_date)
+# print(sentence)
